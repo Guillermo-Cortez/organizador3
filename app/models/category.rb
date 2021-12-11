dc2600d418699770f345f0726e4c8b5aeb05df2c
guille <guillermo.cortez.rf@gmail.com>
2021-12-11 19:42:43 -0300

    Initial commit- Andando form anidado

---
+++ app/models/category.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Category < ApplicationRecord
+    has_many :tasks
+
+
+    #No pasa info en blanco
+    validates :name, :description, presence: true
+    #Validacion de unicidad en cuanto a cierto campo
+    validates :name, uniqueness: { case_sensitive: false }
+end
---
+++ app/models/task.rb
@@ -0,0 +1,37 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  due_date    :date
+#  category_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Task < ApplicationRecord
+  belongs_to :category
+  belongs_to :owner, class_name: "User"
+  has_many :participating_users, class_name: "Participant"
+  has_many :participants, through: :participating_users, source: :user
+
+  validates :participating_users, presence: true
+
+
+  #No pasa info en blanco
+  validates :name, :description, presence: true
+  #Validacion de unicidad en cuanto a cierto campo
+  validates :name, uniqueness: { case_sensitive: false }
+  #Validacion personalizada
+  validate :due_date_validity
+
+  #Permite guardar atributos de modelos relacionados en esta caso de la relacion con participant
+  accepts_nested_attributes_for :participating_users, allow_destroy: true
+
+  def due_date_validity
+    return if due_date.blank?
+    return if due_date > Date.today
+    errors.add :due_date, I18n.t('task.errors.invalid_due_date')
+  end
+end
